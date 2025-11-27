cdb3425116d3cff8d5a287a405f34a82b07c2609
Sundaram singh <155410013+Sundaramsingh21@users.noreply.github.com>
2025-11-27 10:09:43 +0530

    Add files via upload

---
+++ SYP_System/src/main/java/com/example/SYP_System/service/InvoiceService.java
@@ -0,0 +1,19 @@
+package com.example.SYP_System.service;
+
+import java.util.List;
+
+import com.example.SYP_System.dto.invoiceDto.InvoiceRequestDTO;
+import com.example.SYP_System.dto.invoiceDto.InvoiceResponseDTO;
+
+public interface InvoiceService {
+
+    InvoiceResponseDTO createInvoice(InvoiceRequestDTO dto);
+
+    InvoiceResponseDTO getInvoiceById(Long id);
+
+    List<InvoiceResponseDTO> getAllInvoices();
+
+    InvoiceResponseDTO updateInvoice(Long id, InvoiceRequestDTO dto);
+
+    void deleteInvoice(Long id);
+}
